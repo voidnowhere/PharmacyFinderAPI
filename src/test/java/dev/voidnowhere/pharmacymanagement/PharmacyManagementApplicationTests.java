f1a0c5667d88c0e916d2437c7446819938ac0484
voidnowhere <alivnw@outlook.com>
2023-03-18 12:21:03 +0100

    Init

---
+++ src/test/java/dev/voidnowhere/pharmacymanagement/PharmacyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.voidnowhere.pharmacymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
