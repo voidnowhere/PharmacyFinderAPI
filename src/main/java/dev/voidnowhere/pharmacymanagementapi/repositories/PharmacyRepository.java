5d827de96016c9a7b7fc5fd3e724c5e374c5b6cd
voidnowhere <alivnw@outlook.com>
2023-03-20 23:29:50 +0000

    Get available pharmacies and order by nearest

--- src/main/java/dev/voidnowhere/pharmacymanagementapi/repositories/PharmacyRepository.java
+++ src/main/java/dev/voidnowhere/pharmacymanagementapi/repositories/PharmacyRepository.java
@@ -2,11 +2,9 @@ package dev.voidnowhere.pharmacymanagementapi.repositories;
 
 import dev.voidnowhere.pharmacymanagementapi.entities.Pharmacy;
 import dev.voidnowhere.pharmacymanagementapi.entities.custom.PharmacyPosition;
-import dev.voidnowhere.pharmacymanagementapi.enums.WeekDayEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalTime;
 import java.util.List;
 
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
@@ -16,12 +14,14 @@ public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
             "inner join PharmacyWeekDay pwd on pwd.pharmacy.id = p.id " +
             "inner join WeekDay wd on wd.id = pwd.weekDay.id " +
             "where p.zone.id = :zone_id and " +
-            "wd.weekDay = :weekDay and " +
-            "(:time between pwd.firstShiftOpens and pwd.firstShiftCloses or " +
-            ":time between pwd.secondShiftOpens and pwd.secondShiftCloses)"
+            "wd.weekDay = UPPER(DAYNAME(now())) and " +
+            "(CURRENT_TIME() between pwd.firstShiftOpens and pwd.firstShiftCloses or " +
+            "CURRENT_TIME() between pwd.secondShiftOpens and pwd.secondShiftCloses) " +
+            "order by ST_Distance_Sphere(point(p.longitude, p.latitude), point(:currentLongitude, :currentLatitude))"
     )
-    List<Pharmacy> findAllAvailableByZoneId(Long zone_id, LocalTime time, WeekDayEnum weekDay);
+    List<Pharmacy> findAllAvailableByZoneId(Long zone_id, Double currentLatitude, Double currentLongitude);
 
-    @Query("select new dev.voidnowhere.pharmacymanagementapi.entities.custom.PharmacyPosition(p.latitude, p.longitude) from Pharmacy p where p.id = :id")
+    @Query("select new dev.voidnowhere.pharmacymanagementapi.entities.custom.PharmacyPosition(p.latitude, p.longitude) from Pharmacy p " +
+            "where p.id = :id")
     PharmacyPosition getPosition(Long id);
 }
